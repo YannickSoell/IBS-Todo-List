c35cf293bcd13704aaf96eec54748a48f05ed096
Ali Karami <kaali728@yahoo.com>
2021-07-02 00:51:46 +0200

    add mqtt

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ require("./models/Todo");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const mqtt = require("mqtt");
+const cache = require("memory-cache");
 const authRoute = require("./route/authRoute");
 const todoRoute = require("./route/todoRoute");
 const notifyUser = require("./utils/notifyUser");
@@ -39,3 +41,119 @@ app.get("/", (req, res) => {
 const port = 9998;
 //fcgi.createServer(app).listen(port, () => console.log(`listening on Port ${port}...`));
 app.listen(port, () => console.log(`server started on port ${port}`));
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+/* Backend-URL fuer Alexa-Skill:
+ https://www.ostalbradar.de/node/alexa2mqtt.js?user=6447 //user = canvas-ID
+*/
+var userId = "6447" // Canvas User-ID
+
+var connectOptions = {
+	host: "www.ostalbradar.de",
+	port: 8883,
+	protocol: "mqtts",
+	username: "6447", // Canvas User-ID
+	password: "RPo6zINUyveBhOrQ0gFmT9Yvrp0=" // Canvas persistent-ID
+};
+
+var userSession = {
+	zustand: 0,
+	todoText: "",
+	todoDate: "",
+};
+
+function onMessage(topic, message) {
+	var alexaOBJ = JSON.parse('' + message);
+	responseText = "Hallo welt";
+	var intent;
+	var responseNummer;
+	var flag = false;
+	var todos;
+	var sessionId = alexaOBJ.session.sessionId;
+	//var intents = ["gettodo","maketodo","todotext", "tododate", "todotime"];
+
+  	// Session
+	if(alexaOBJ.session.new) {
+		userSession = {
+			zustand: 0,
+			todoText: "",
+			todoDate: "",
+		};
+		cache.put(sessionId, userSession);
+	}
+	else {
+		userSession = cache.get(sessionId);
+	}
+
+
+	if(alexaOBJ.request.type === "LaunchRequest"){
+		console.log("LaunchRequest")
+		userSession.zustand = 0;
+		cache.put(sessionId, userSession);
+	}else if(alexaOBJ.request.type === 'IntentRequest'){	
+		intent = alexaOBJ.request.intent.name;
+		//intentsNummer = intents.indexOf(intentName);	
+	}else if(alexaOBJ.request.type === "SessionEndedRequest"){
+		cache.del(sessionId);
+	}
+
+
+	/* responseNummer:
+					0 ist Launchreq
+					1 ist intents gettodo
+					2 ist intents maketodo
+					3 ist intents todotext
+					4 ist intents tododate
+					5 ist intents todotime
+	*/
+	
+	var responses = [`Willkommen zurück bei deiner Todo List. Was möchtest du machen, die heute fälligen Todos vorlesen oder ein neues Todo erstellen?`,
+					 `Deine heutige todos sind ${todos}`,
+					 `Was möchtest du erledigen?`,
+           			 `Ok. Wann möchtest du das machen?`,
+                  	 `Alles klar. Und um wie viel Uhr?`
+					];
+	
+	responseText = responses[responseNummer]
+
+    // Response
+	var response = {
+			response: {
+				outputSpeech: {
+					text: responseText,
+					type: "PlainText"
+				},
+				shouldEndSession: flag
+			},
+			version: "1.0"
+	};
+	
+
+	console.log(topic, '' + message);
+	client.publish(
+		topic.replace("fr","to"), JSON.stringify(response));
+}
+
+
+
+(async function main() {
+	console.log("Go");
+	client = mqtt.connect(connectOptions)
+	.on("connect", function() {
+		console.log("mqtt connected");
+		client.on('message', onMessage);
+		client.subscribe("mqttfetch/alexa2mqtt/" + 6447 + "/fr/+"); // canvas-ID
+	}
+		)
+})();
